e56331069ecb385860fb6a776295ddf6a81e2120
Brian Pollack <brian.pollack@cern.ch>
2014-03-07 14:02:12 -0600

    batch now smarter for separate shit

--- batchLimits.py
+++ batchLimits.py
@@ -22,9 +22,11 @@ for mass in massList:
   os.system('rm limitOutput/res/*')
 
   os.system('tar cvzf limitFiles.tgz Signal* Card* hzg*')
+  if mode == 'Combo':
+    cardName = '_'.join(['hzg','FullCombo','M'+mass,suffix])+'.txt'
 
-  consub = open('submit.cmd','w')
-  consub.write('''
+    consub = open('submit.cmd','w')
+    consub.write('''
 Universe                = vanilla
 Notify_user             = brian.pollack@cern.ch
 Notification            = Error
@@ -41,10 +43,45 @@ should_transfer_files   = YES
 when_to_transfer_output = ON_EXIT
 transfer_input_files    = limitFiles.tgz
 Queue
-'''.format(mass,suffix,mode,outPutFolder+'/limitOutput/'))
-  consub.close()
+  '''.format(mass,suffix,cardName,outPutFolder+'/limitOutput/'))
+    consub.close()
 
-  os.system('condor_submit submit.cmd')
-  os.system('rm submit.cmd')
+    os.system('condor_submit submit.cmd')
+    os.system('rm submit.cmd')
+  else:
+    leptonList = ['mu','el']
+    yearList = ['2012','2011']
+    catListBig = ['1','2','3','4','5','6','7','8','9']
+    catListSmall = ['1','2','3','4','5']
+    for lepton in leptonList:
+      for year in yearList:
+        for cat in catListSmall:
+          if cat == '5' and year == '2011' and lepton == 'el': myLepton = 'all'
+          elif cat == '5' and year == '2011' and lepton == 'mu': continue
+          else: myLepton = lepton
+          cardName = '_'.join(['hzg',myLepton,year,'cat'+cat,'M'+mass,suffix])+'.txt'
+          consub = open('submit.cmd','w')
+          consub.write('''
+Universe                = vanilla
+Notify_user             = brian.pollack@cern.ch
+Notification            = Error
+Executable              = /tthome/bpollack/CMSSW_6_1_1/src/BiasAndLimits/limitExe.py
+Arguments               = {0} {1} {2} {3}
+Rank                    = Mips
+Requirements            = (OpSys == "LINUX") && (Disk >= DiskUsage) && ((Memory * 1024) >= ImageSize) && (HasFileTransfer)
++LENGTH                 = "LONG"
+GetEnv                  = True
+Input                   = /dev/null
+Output                  = limitOutput/res/job_{0}_{2}.out
+Error                   = limitOutput/res/job_{0}_{2}.err
+should_transfer_files   = YES
+when_to_transfer_output = ON_EXIT
+transfer_input_files    = limitFiles.tgz
+Queue
+          '''.format(mass,suffix,cardName,outPutFolder+'/limitOutput/'))
+          consub.close()
+
+          os.system('condor_submit submit.cmd')
+          os.system('rm submit.cmd')
 
 
--- limitExe.py
+++ limitExe.py
@@ -8,24 +8,11 @@ os.system('tar xvzf limitFiles.tgz')
 mass=sys.argv[1]
 suffix=sys.argv[2]
 outPutFolder = sys.argv[4]
-if sys.argv[3].lower() == 'combo':
-  cardName = '_'.join(['hzg','FullCombo','M'+mass,suffix])+'.txt'
-  outputName = outPutFolder+'_'.join(['Output','FullCombo','M'+mass,suffix])+'.txt'
-  print 'running limitProducer, FullCombo,  mass: {0}'.format(mass)
-  os.system('combine -M Asymptotic '+cardName+' > '+outputName)
-else:
-  leptonList = ['mu','el']
-  yearList = ['2012','2011']
-  catListBig = ['1','2','3','4','5','6','7','8','9']
-  catListSmall = ['1','2','3','4','5']
-  for lepton in leptonList:
-    for year in yearList:
-      for cat in catListSmall:
-        cardName = '_'.join(['hzg',lepton,year,'cat'+cat,'M'+mass,suffix])+'.txt'
-        outputName = '_'.join(['Output',lepton,year,'cat'+cat,'M'+mass,suffix])+'.txt'
-        print 'running limitProducer, {1}, {2}, cat{3}, mass: {0}'.format(mass,lepton,year,cat)
-        os.system('combine -M Asymptotic '+cardName+' > '+outputName)
 
+cardName = sys.argv[3]
+outputName = outPutFolder+'Output'+cardName[3:]
+print 'running limitProducer, {0},  mass: {1}'.format(cardName,mass)
+os.system('combine -M Asymptotic '+cardName+' > '+outputName)
 
 
 print 'Done'
